15b68a60687933ecef54dc4f8a1e44662a6521cb
Maximilian <maxel2804@gmail.com>
2020-08-23 16:43:09 +0200

    NavBar

--- app/src/main/java/com/example/informationappjava/ui/campus/CampusViewModel.java
+++ app/src/main/java/com/example/informationappjava/ui/campus/CampusViewModel.java
@@ -1,16 +1,16 @@
-package com.example.informationappjava.ui.slideshow;
+package com.example.informationappjava.ui.campus;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class CampusViewModel extends ViewModel {
 
   private MutableLiveData<String> mText;
 
-  public SlideshowViewModel() {
+  public CampusViewModel() {
     mText = new MutableLiveData<>();
-    mText.setValue("This is slideshow fragment");
+    mText.setValue("This is gallery fragment");
   }
 
   public LiveData<String> getText() {
--- app/src/main/java/com/example/informationappjava/ui/institute/InstituteViewModel.java
+++ app/src/main/java/com/example/informationappjava/ui/institute/InstituteViewModel.java
@@ -1,16 +1,16 @@
-package com.example.informationappjava.ui.gallery;
+package com.example.informationappjava.ui.institute;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class InstituteViewModel extends ViewModel {
 
   private MutableLiveData<String> mText;
 
-  public GalleryViewModel() {
+  public InstituteViewModel() {
     mText = new MutableLiveData<>();
-    mText.setValue("This is gallery fragment");
+    mText.setValue("This is slideshow fragment");
   }
 
   public LiveData<String> getText() {
