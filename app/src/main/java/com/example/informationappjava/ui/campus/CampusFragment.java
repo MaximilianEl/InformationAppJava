d7359d717c48fcf1a40d7779dcd0c1c05fbd421f
Maximilian <maxel2804@gmail.com>
2020-08-24 22:26:43 +0200

    Campus Fragment update

--- app/src/main/java/com/example/informationappjava/ui/campus/CampusFragment.java
+++ app/src/main/java/com/example/informationappjava/ui/campus/CampusFragment.java
@@ -1,13 +1,18 @@
 package com.example.informationappjava.ui.campus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.ViewPager;
@@ -19,6 +24,7 @@ public class CampusFragment extends Fragment {
   private CampusViewModel mViewModel;
   ViewPager viewPager;
   ImageAdapter imageAdapter;
+  Button campMapButton;
 
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -28,6 +34,20 @@ public class CampusFragment extends Fragment {
     viewPager = (ViewPager) view.findViewById(R.id.camppager);
     imageAdapter = new ImageAdapter(this.getActivity());
     viewPager.setAdapter(imageAdapter);
+
+    campMapButton = view.findViewById(R.id.campus_map_button);
+    campMapButton.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Fragment fragment = new CampusMapFragment();
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.campus_map_fragment, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+      }
+    });
+
     return view;
   }
 
