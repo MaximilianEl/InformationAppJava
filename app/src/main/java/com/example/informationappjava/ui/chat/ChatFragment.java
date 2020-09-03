7a10d9259a141a8d60a0ee102420a2f295c5c9d8
RealKrichter <RealKrichter@users.noreply.github.com>
2020-09-03 17:12:27 +0200

    OHHH YEAH DIESER BUTTON

--- app/src/main/java/com/example/informationappjava/ui/chat/ChatFragment.java
+++ app/src/main/java/com/example/informationappjava/ui/chat/ChatFragment.java
@@ -1,7 +1,11 @@
 package com.example.informationappjava.ui.chat;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.view.SubMenu;
+import android.widget.Button;
 import android.widget.EditText;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -11,35 +15,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.informationappjava.R;
+
 import java.util.Objects;
 
 public class ChatFragment extends Fragment {
 
-  private ChatViewModel mViewModel;
+    private ChatViewModel mViewModel;
 
-  public static ChatFragment newInstance() {
-    return new ChatFragment();
-  }
+    public static ChatFragment newInstance() {
+        return new ChatFragment();
+    }
 
-  @Override
-  public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container,
-      @Nullable Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_campus, container, false);
-    final EditText editText = view.findViewById(R.id.chat_enterName);
-    view.findViewById(R.id.chat_enterButton).setOnClickListener(v -> {
-      Intent intent = new Intent(this, ChatActivity.class);
-      intent.putExtra("name", editText.getText().toString());
-      startActivity(intent);
-    });
-    return view;
-  }
 
-  @Override
-  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-    super.onActivityCreated(savedInstanceState);
-    mViewModel = ViewModelProviders.of(this).get(ChatViewModel.class);
-    // TODO: Use the ViewModel
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
 
-  }
+        final EditText editText = view.findViewById(R.id.chat_enterName);
+
+        Button enterbutton = view.findViewById(R.id.chat_enterButton);
+
+        enterbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              Intent in = new Intent(getActivity(), ChatActivity.class);
+              startActivity(in);
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ChatViewModel.class);
+        // TODO: Use the ViewModel
+    }
 
 }
