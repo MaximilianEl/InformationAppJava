8128447ba40319398aa96726e0533dab970af943
Maximilian <maxel2804@gmail.com>
2020-09-18 15:32:24 +0200

    Chat

--- app/src/main/java/com/example/informationappjava/ui/chat/contactlist/ContactListActivity.java
+++ app/src/main/java/com/example/informationappjava/ui/chat/contactlist/ContactListActivity.java
@@ -1,5 +1,6 @@
 package com.example.informationappjava.ui.chat.contactlist;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +11,6 @@ import android.widget.EditText;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.informationappjava.ui.chat.MeActivity;
@@ -25,119 +25,129 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.example.informationappjava.R;
 
-public class ContactListActivity extends AppCompatActivity implements ContactListAdapter.OnItemClickListener, ContactListAdapter.OnItemLongClickListener {
+public class ContactListActivity extends AppCompatActivity implements
+    ContactListAdapter.OnItemClickListener, ContactListAdapter.OnItemLongClickListener {
 
-    private RecyclerView contactListRecyclerView;
-    ContactListAdapter mAdapter;
-    private static final String LOGTAG = "ContactListActivity";
+  private RecyclerView contactListRecyclerView;
+  ContactListAdapter mAdapter;
+  private static final String LOGTAG = "ContactListActivity";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_list);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_contact_list);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-        FloatingActionButton newcontactbutton = findViewById(R.id.newcontactbutton);
-        newcontactbutton.setBackgroundTintList(getResources().getColorStateList(R.color.colorPrimary));
-        newcontactbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // A Dialog should pop up but it doesn't
-                addContact();
+    FloatingActionButton newContactButton = findViewById(R.id.newcontactbutton);
+    newContactButton.setBackgroundTintList(getResources().getColorStateList(R.color.colorPrimary));
+    newContactButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        // A Dialog should pop up but it doesn't
+        addContact();
+      }
+    });
+
+    contactListRecyclerView = findViewById(R.id.contact_list_recycler_view);
+    contactListRecyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+
+    mAdapter = new ContactListAdapter(getApplicationContext());
+    mAdapter.setmOnItemClickListener(this);
+    mAdapter.setmOnItemLongClickListener(this);
+    contactListRecyclerView.setAdapter(mAdapter);
+  }
+
+  private void addContact() {
+
+    AlertDialog.Builder builder = new AlertDialog.Builder(ContactListActivity.this);
+    builder.setTitle(R.string.add_contact_label_text);
+    final EditText input = new EditText(this);
+    input.setInputType(InputType.TYPE_CLASS_TEXT);
+    builder.setView(input);
+
+
+
+    builder.setPositiveButton(R.string.add_contact_confirm_text,
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            Log.d(LOGTAG, "User clicked on OK");
+            if (ContactModel.get(getApplicationContext()).addContact(
+                new Contact(input.getText().toString(), Contact.SubscriptionType.NONE_NONE))) {
+              mAdapter.onContactCountChange();
+              Log.d(LOGTAG, "Contact added successfully");
+            } else {
+              Log.d(LOGTAG, "Could not add contact");
             }
+          }
+        });
+    builder
+        .setNegativeButton(R.string.add_contact_cancel_text, new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialogInterface, int i) {
+            Log.d(LOGTAG, "User clicked on Cancel");
+            dialogInterface.cancel();
+          }
         });
 
-        contactListRecyclerView = findViewById(R.id.contact_list_recycler_view);
-        contactListRecyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+    AlertDialog alertDialog = builder.create();
+    alertDialog.show();
+  }
 
-        mAdapter = new ContactListAdapter(getApplicationContext());
-        mAdapter.setmOnItemClickListener(this);
-        mAdapter.setmOnItemLongClickListener(this);
-        contactListRecyclerView.setAdapter(mAdapter);
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.activity_me_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    if (item.getItemId() == R.id.me) {
+      Intent i = new Intent(ContactListActivity.this, MeActivity.class);
+      startActivity(i);
     }
+    return super.onOptionsItemSelected(item);
+  }
 
-    private void addContact() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(ContactListActivity.this);
-        builder.setTitle(R.string.add_contact_label_text);
-        final EditText input = new EditText(this);
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        builder.setView(input);
 
-        builder.setPositiveButton(R.string.add_contact_confirm_text, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Log.d(LOGTAG, "User clicked on OK");
-                if (ContactModel.get(getApplicationContext()).addContact(new Contact(input.getText().toString(), Contact.SubscriptionType.NONE_NONE))) {
-                    mAdapter.onContactCountChange();
-                    Log.d(LOGTAG, "Contact added successfully");
-                } else {
-                    Log.d(LOGTAG, "Could not add contact");
-                }
+  @Override
+  public void onItemClick(String contactJid) {
+    Intent intent = new Intent(ContactListActivity.this, ChatViewActivity.class);
+    intent.putExtra("contact_jid", contactJid);
+    startActivity(intent);
+  }
+
+  @Override
+  public void onItemLongClick(final int uniqueId, final String contactJid, View anchor) {
+
+    PopupMenu popup = new PopupMenu(ContactListActivity.this, anchor, Gravity.CENTER);
+
+    popup.getMenuInflater().inflate(R.menu.contact_list_popup_menu, popup.getMenu());
+
+    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+      @Override
+      public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+          case R.id.delete_contact:
+            if (ContactModel.get(getApplicationContext()).deleteContact(uniqueId)) {
+              mAdapter.onContactCountChange();
+              Toast.makeText(ContactListActivity.this,
+                  "Contact deleted successfully ",
+                  Toast.LENGTH_SHORT).show();
             }
-        });
-        builder.setNegativeButton(R.string.add_contact_cancel_text, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Log.d(LOGTAG, "User clicked on Cancel");
-                dialogInterface.cancel();
-            }
-        });
-    }
+            break;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.activity_me_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.me) {
-            Intent i = new Intent(ContactListActivity.this, MeActivity.class);
-            startActivity(i);
+          case R.id.contact_details:
+            Toast.makeText(ContactListActivity.this,
+                "You Long Clicked to see : " + contactJid + " 's contact details",
+                Toast.LENGTH_SHORT).show();
+            return true;
         }
-        return super.onOptionsItemSelected(item);
-    }
+        return true;
+      }
+    });
 
-
-    @Override
-    public void onItemClick(String contactJid) {
-        Intent intent = new Intent(ContactListActivity.this, ChatViewActivity.class);
-        intent.putExtra("contact_jid", contactJid);
-        startActivity(intent);
-    }
-
-    @Override
-    public void onItemLongClick(final int uniqueId,final String contactJid, View anchor) {
-
-        PopupMenu popup = new PopupMenu(ContactListActivity.this, anchor, Gravity.CENTER);
-
-        popup.getMenuInflater().inflate(R.menu.contact_list_popup_menu, popup.getMenu());
-
-        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.delete_contact:
-                        if (ContactModel.get(getApplicationContext()).deleteContact(uniqueId)) {
-                            mAdapter.onContactCountChange();
-                            Toast.makeText(ContactListActivity.this,
-                                    "Contact deleted successfully ",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                        break;
-
-                    case R.id.contact_details:
-                        Toast.makeText(ContactListActivity.this,
-                                "You Long Clicked to see : " + contactJid + " 's contact details",
-                                Toast.LENGTH_SHORT).show();
-                        return true;
-                }
-                return true;
-            }
-        });
-
-    }
+  }
 }
