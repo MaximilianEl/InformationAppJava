73e77e576a4573d0f0a50b44ab4eca9f4d5311e5
Maximilian <maxel2804@gmail.com>
2020-10-08 22:04:17 +0200

    Grid

--- app/src/main/java/com/example/informationappjava/ui/institute/InstituteContextActivity.java
+++ app/src/main/java/com/example/informationappjava/ui/institute/InstituteContextActivity.java
@@ -1,30 +1,106 @@
 package com.example.informationappjava.ui.institute;
 
 import android.content.Intent;
-import android.widget.TextView;
+import android.net.Uri;
+import android.view.View;
+import android.view.View.OnClickListener;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.cardview.widget.CardView;
 import com.example.informationappjava.R;
-import com.example.informationappjava.ui.institute.PersonAdapter.OnItemClickListener;
-import java.util.ArrayList;
-import java.util.List;
 
 public class InstituteContextActivity extends AppCompatActivity {
 
-  private String course = "course";
-  private String freshmanHelp = "freshmanHelp";
-  private String event = "event";
-  private RecyclerView recyclerView;
-  private PersonAdapter adapter;
-  private RecyclerView.LayoutManager layoutManager;
+  private CardView cardViewMBau;
+  private CardView cardViewBWL;
+  private CardView cardViewBInf;
+  private CardView cardViewBIng;
+  private CardView cardViewMBA;
+  private CardView cardViewBIngEne;
+  private CardView cardViewMAT;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_institute_context);
+
+    cardViewMBau = findViewById(R.id.ins_masch);
+    cardViewMBau.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String url = "https://www.hs-osnabrueck.de/studium/studienangebot/bachelor/allgemeiner-maschinenbau-bsc-standort-lingen-ems/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+      }
+    });
+
+    cardViewBWL = findViewById(R.id.ins_bwl);
+    cardViewBWL.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String url = "https://www.hs-osnabrueck.de/studium/studienangebot/bachelor/betriebswirtschaft-und-management-ba-standort-lingen-ems/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+      }
+    });
+
+    cardViewBInf = findViewById(R.id.ins_infor);
+    cardViewBInf.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String url = "https://www.hs-osnabrueck.de/studium/studienangebot/bachelor/wirtschaftsinformatik-bsc-standort-lingen-ems/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+      }
+    });
+
+    cardViewBIng = findViewById(R.id.ins_ing);
+    cardViewBIng.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String url = "https://www.hs-osnabrueck.de/studium/studienangebot/bachelor/wirtschaftsingenieurwesen-bsc-standort-lingen-ems/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+      }
+    });
+
+    cardViewMBA = findViewById(R.id.ins_mba);
+    cardViewMBA.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String url = "https://www.hs-osnabrueck.de/studium/studienangebot/master/wirtschaftsingenieurwesen-mba-berufsbegleitend/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+      }
+    });
+
+    cardViewBIngEne = findViewById(R.id.ins_engergy);
+    cardViewBIngEne.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String url = "https://www.hs-osnabrueck.de/studium/studienangebot/master/wirtschaftsingenieurwesen-energiewirtschaft-msc-standort-lingen-ems/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+      }
+    });
+
+    cardViewMAT = findViewById(R.id.ins_mut);
+    cardViewMAT.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String url = "https://www.hs-osnabrueck.de/studium/studienangebot/master/management-und-technik-msc/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+      }
+    });
 
   }
 }
