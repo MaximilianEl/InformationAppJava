352226fda6773c496a46b06712270b3eda307da6
RealKrichter <RealKrichter@users.noreply.github.com>
2020-10-08 23:10:33 +0200

    Test denke ich

---
+++ app/src/androidTest/java/com/example/informationappjava/TestSettings.java
@@ -0,0 +1,41 @@
+package com.example.informationappjava;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+import com.example.informationappjava.ui.home.HomeFragment;
+import com.example.informationappjava.ui.imprint.ImprintFragment;
+import com.example.informationappjava.ui.institute.InstituteContextActivity;
+import com.example.informationappjava.ui.settings.SettingsActivity;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class TestSettings {
+
+    @Rule
+    public ActivityTestRule<SettingsActivity> activityRule = new ActivityTestRule<>(
+            SettingsActivity.class);
+
+    @Test
+    public void Testsettings_daynight() {
+        onView(withId(R.id.dayNight)).perform(click());
+    }
+
+    @Test
+    public void Testsettings_language(){
+        onView(withId(R.id.language)).perform(click());
+    }
+
+    @Test
+    public void Testsettings_myswitch(){
+        onView(withId(R.id.myswitch)).perform(click());
+    }
+}
