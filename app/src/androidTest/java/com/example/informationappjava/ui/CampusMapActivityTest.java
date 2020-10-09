a9e303f39d3a83ff8d9002e11c9ec7d7bb05e4ff
Maximilian <maxel2804@gmail.com>
2020-10-09 15:21:14 +0200

    Test

---
+++ app/src/androidTest/java/com/example/informationappjava/ui/CampusMapActivityTest.java
@@ -0,0 +1,13 @@
+package com.example.informationappjava.ui;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import com.example.informationappjava.ui.campus.CampusMapActivity;
+import org.junit.Rule;
+
+public class CampusMapActivityTest {
+
+  @Rule
+  public ActivityScenarioRule<CampusMapActivity> activityScenarioRule = new ActivityScenarioRule<>(
+      CampusMapActivity.class);
+
+}
