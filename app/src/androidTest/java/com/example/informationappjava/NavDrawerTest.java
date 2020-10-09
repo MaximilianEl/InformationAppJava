9e9975987ba380191a0f8622dcb6944aab51f714
Maximilian <maxel2804@gmail.com>
2020-10-09 13:05:41 +0200

    Test

--- app/src/androidTest/java/com/example/informationappjava/NavDrawerTest.java
+++ app/src/androidTest/java/com/example/informationappjava/NavDrawerTest.java
@@ -1,6 +1,7 @@
 package com.example.informationappjava;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -11,7 +12,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
-import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.*;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.rule.ActivityTestRule;
 import org.hamcrest.Matcher;
@@ -24,20 +25,54 @@ public class NavDrawerTest {
   public ActivityScenarioRule<NavDrawer> activityTestRule = new ActivityScenarioRule<>(
       NavDrawer.class);
 
-//  @Test
-//  public void open_Close_Drawer_Layout() {
-//    onView(withId(R.id.nav_view)).perform(act);
-//    try {
-//      Thread.sleep(5000);
-//    } catch (InterruptedException e) {
-//      e.printStackTrace();
-//    }
-//    onView(withId(R.id.drawer_layout)).perform(actionCloseDrawer());
-//    try {
-//      Thread.sleep(5000);
-//    } catch (InterruptedException e) {
-//      e.printStackTrace();
-//    }
-//  }
+  @Test
+  public void open_Close_Drawer_Layout() {
+    onView(withId(R.id.drawer_layout)).perform(actionOpenDrawer());
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    onView(withId(R.id.drawer_layout)).perform(actionCloseDrawer());
+  }
+
+  private static ViewAction actionOpenDrawer() {
+    return new ViewAction() {
+      @Override
+      public Matcher<View> getConstraints() {
+        return isAssignableFrom(DrawerLayout.class);
+      }
+
+      @Override
+      public String getDescription() {
+        return "open drawer";
+      }
+
+      @Override
+      public void perform(UiController uiController, View view) {
+        ((DrawerLayout) view).openDrawer(GravityCompat.START);
+      }
+    };
+  }
+
+  private static ViewAction actionCloseDrawer() {
+    return new ViewAction() {
+      @Override
+      public Matcher<View> getConstraints() {
+        return isAssignableFrom(DrawerLayout.class);
+      }
+
+      @Override
+      public String getDescription() {
+        return "close drawer";
+      }
+
+      @Override
+      public void perform(UiController uiController, View view) {
+        ((DrawerLayout) view).closeDrawer(GravityCompat.START);
+      }
+    };
+  }
+
 }
 
