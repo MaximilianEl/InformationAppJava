635286539d54b50a3aac8ad4cfced4258b8303a5
Maximilian <maxel2804@gmail.com>
2020-10-10 19:23:09 +0200

    Comment

--- app/src/main/java/com/example/informationappjava/ui/registration/RegistrationActivity.java
+++ app/src/main/java/com/example/informationappjava/ui/registration/RegistrationActivity.java
@@ -6,34 +6,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 /**
- *
+ * RegistrationActivity called from the LoginActivity.
  */
 public class RegistrationActivity extends AppCompatActivity {
 
-    private WebView webView;
+  private WebView webView;
 
-    /**
-     * @param savedInstanceState
-     */
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        webView = new WebView(this);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://sep-01.lin.hs-osnabrueck.de:9090/plugins/registration/sign-up.jsp");
-        setContentView(webView);
+  /**
+   * onCreate function is used to set the content view to the specific Link.
+   *
+   * @param savedInstanceState
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    webView = new WebView(this);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.setWebViewClient(new WebViewClient());
+    webView.loadUrl("http://sep-01.lin.hs-osnabrueck.de:9090/plugins/registration/sign-up.jsp");
+    setContentView(webView);
+  }
+
+  /**
+   * onDestroy is called if the webView is not null and destroys the webView.
+   */
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+
+    if (webView != null) {
+      webView.destroy();
     }
-
-    /**
-     *
-     */
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if (webView != null) {
-            webView.destroy();
-        }
-    }
+  }
 }
