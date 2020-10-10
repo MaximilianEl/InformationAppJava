5d9be613427a11c08a8d34b69afaf2e9b21a2be4
RealKrichter <RealKrichter@users.noreply.github.com>
2020-10-10 19:12:02 +0200

    Doccomments

--- app/src/main/java/com/example/informationappjava/persistence/ChatMessageCursorWrapper.java
+++ app/src/main/java/com/example/informationappjava/persistence/ChatMessageCursorWrapper.java
@@ -5,37 +5,41 @@ import android.database.CursorWrapper;
 import com.example.informationappjava.ui.chat.view.model.ChatMessage;
 
 /**
- *
+ * The ChatMessageCursorWrapper class is used to access the Chatmessage information if the Cursor accesses it.
  */
 public class ChatMessageCursorWrapper extends CursorWrapper {
 
-  /**
-   * @param cursor
-   */
-  public ChatMessageCursorWrapper(Cursor cursor) {
-    super(cursor);
-  }
-
-  /**
-   * @return
-   */
-  public ChatMessage getChatMessage() {
-    String message = getString(getColumnIndex(ChatMessage.Cols.MESSAGE));
-    long timestamp = getLong(getColumnIndex(ChatMessage.Cols.TIMESTAMP));
-    String messagetype = getString(getColumnIndex(ChatMessage.Cols.MESSAGE_TYPE));
-    String counterpartJid = getString(getColumnIndex(ChatMessage.Cols.CONTACT_JID));
-    int uniqueId = getInt(getColumnIndex(ChatMessage.Cols.CHAT_MESSAGE_UNIQUE_ID));
-
-    ChatMessage.Type chatMessageType = null;
-
-    if (messagetype.equals("SENT")) {
-      chatMessageType = ChatMessage.Type.SENT;
-    } else if (messagetype.equals("RECEIVED")) {
-      chatMessageType = ChatMessage.Type.RECEIVED;
+    /**
+     * This is a constructor to call upon the ChatMessageCursorWrapper class.
+     *
+     * @param cursor
+     */
+    public ChatMessageCursorWrapper(Cursor cursor) {
+        super(cursor);
     }
-    ChatMessage chatMessage = new ChatMessage(message, timestamp, chatMessageType, counterpartJid);
-    chatMessage.setPersistID(uniqueId);
 
-    return chatMessage;
-  }
+    /**
+     * This function is used to get the Chatmessage Informations for a specific Chatmessage like the messageType.
+     *
+     * @return
+     */
+    public ChatMessage getChatMessage() {
+        String message = getString(getColumnIndex(ChatMessage.Cols.MESSAGE));
+        long timestamp = getLong(getColumnIndex(ChatMessage.Cols.TIMESTAMP));
+        String messagetype = getString(getColumnIndex(ChatMessage.Cols.MESSAGE_TYPE));
+        String counterpartJid = getString(getColumnIndex(ChatMessage.Cols.CONTACT_JID));
+        int uniqueId = getInt(getColumnIndex(ChatMessage.Cols.CHAT_MESSAGE_UNIQUE_ID));
+
+        ChatMessage.Type chatMessageType = null;
+
+        if (messagetype.equals("SENT")) {
+            chatMessageType = ChatMessage.Type.SENT;
+        } else if (messagetype.equals("RECEIVED")) {
+            chatMessageType = ChatMessage.Type.RECEIVED;
+        }
+        ChatMessage chatMessage = new ChatMessage(message, timestamp, chatMessageType, counterpartJid);
+        chatMessage.setPersistID(uniqueId);
+
+        return chatMessage;
+    }
 }
--- app/src/main/java/com/example/informationappjava/persistence/ContactCursorWrapper.java
+++ app/src/main/java/com/example/informationappjava/persistence/ContactCursorWrapper.java
@@ -7,51 +7,54 @@ import com.example.informationappjava.ui.chat.contacts.model.Contact.Cols;
 import com.example.informationappjava.ui.chat.contacts.model.Contact.SubscriptionType;
 
 /**
- *
+ * The ContactCursorWrapper class is used to access the Contact information if the Cursor accesses it.
  */
 public class ContactCursorWrapper extends CursorWrapper {
 
-  /**
-   *
-   * @param cursor
-   */
-  public ContactCursorWrapper(Cursor cursor) {
-    super(cursor);
-  }
-
-  /**
-   * @return
-   */
-  public Contact getContact() {
-
-    String subscriptionTypeString = getString(getColumnIndex(Contact.Cols.SUBSCRIPTION_TYPE));
-    String jid = getString(getColumnIndex(Contact.Cols.CONTACT_JID));
-    int contactUniqueId = getInt(getColumnIndex(Contact.Cols.CONTACT_UNIQUE_ID));
-    String profileImagePath = getString(getColumnIndex(Contact.Cols.PROFILE_IMAGE_PATH));
-
-    int pendingFromInt = getInt(getColumnIndex(Cols.PENDING_STATUS_FROM));
-    int pendingToInt = getInt(getColumnIndex(Cols.PENDING_STATUS_TO));
-    int onlineStatusInt = getInt(getColumnIndex(Cols.ONLINE_STATUS));
-
-    Contact.SubscriptionType subscriptionType = null;
-
-    if (subscriptionTypeString.equals("NONE")) {
-      subscriptionType = SubscriptionType.NONE;
-    } else if (subscriptionTypeString.equals("FROM")) {
-      subscriptionType = SubscriptionType.FROM;
-    } else if (subscriptionTypeString.equals("TO")) {
-      subscriptionType = SubscriptionType.TO;
-    } else if (subscriptionTypeString.equals("BOTH")) {
-      subscriptionType = SubscriptionType.BOTH;
+    /**
+     * This is a constructor to call upon the ContactCursorWrapper class.
+     *
+     * @param cursor
+     */
+    public ContactCursorWrapper(Cursor cursor) {
+        super(cursor);
     }
 
-    Contact contact = new Contact(jid, subscriptionType);
-    contact.setPersistID(contactUniqueId);
-    contact.setProfileImagePath(profileImagePath);
+    /**
+     * This function is used to get the Contact Informations for a specific Contact like his Subscription status.
+     *
+     * @return
+     */
+    public Contact getContact() {
 
-    contact.setPendingFrom(pendingFromInt != 0);
-    contact.setPendingTo(pendingToInt != 0);
-    contact.setOnlineStatus(onlineStatusInt != 0);
-    return contact;
-  }
+        String subscriptionTypeString = getString(getColumnIndex(Contact.Cols.SUBSCRIPTION_TYPE));
+        String jid = getString(getColumnIndex(Contact.Cols.CONTACT_JID));
+        int contactUniqueId = getInt(getColumnIndex(Contact.Cols.CONTACT_UNIQUE_ID));
+        String profileImagePath = getString(getColumnIndex(Contact.Cols.PROFILE_IMAGE_PATH));
+
+        int pendingFromInt = getInt(getColumnIndex(Cols.PENDING_STATUS_FROM));
+        int pendingToInt = getInt(getColumnIndex(Cols.PENDING_STATUS_TO));
+        int onlineStatusInt = getInt(getColumnIndex(Cols.ONLINE_STATUS));
+
+        Contact.SubscriptionType subscriptionType = null;
+
+        if (subscriptionTypeString.equals("NONE")) {
+            subscriptionType = SubscriptionType.NONE;
+        } else if (subscriptionTypeString.equals("FROM")) {
+            subscriptionType = SubscriptionType.FROM;
+        } else if (subscriptionTypeString.equals("TO")) {
+            subscriptionType = SubscriptionType.TO;
+        } else if (subscriptionTypeString.equals("BOTH")) {
+            subscriptionType = SubscriptionType.BOTH;
+        }
+
+        Contact contact = new Contact(jid, subscriptionType);
+        contact.setPersistID(contactUniqueId);
+        contact.setProfileImagePath(profileImagePath);
+
+        contact.setPendingFrom(pendingFromInt != 0);
+        contact.setPendingTo(pendingToInt != 0);
+        contact.setOnlineStatus(onlineStatusInt != 0);
+        return contact;
+    }
 }
